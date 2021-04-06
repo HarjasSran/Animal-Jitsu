e59aca7bdc93bded4968abdfd4955ee76135de7f
harja <harja@10.16.129.223>
2021-04-06 19:26:09 -0400

    character class

--- FinalProject/src/finalproject/Character.java
+++ FinalProject/src/finalproject/Character.java
@@ -16,9 +16,16 @@ public class Character extends GameObject {
     Image animal;
     ArrayList cards;
     
-    //public Character(){
+    public Character(){
+        
+    }
     
-    //public Character(Color bow, Image Animal, boolean direction, ArrayList cards)
+    public Character(Color bow, Image animal, boolean direction, ArrayList cards){
+        this.bow = bow;
+        this.animal = animal; 
+        this.direction = direction;
+        this.cards = cards;
+    }
     
     public void setBow(Color c){
         bow = c;
@@ -27,7 +34,9 @@ public class Character extends GameObject {
         return bow;
     }
     
-    //public void setAnimal(Image animal)
+    public void setAnimal(Image animal){
+        this.animal = animal;
+    }
     
     public Image getAnimal(){
         return animal;
@@ -37,10 +46,10 @@ public class Character extends GameObject {
         return cards;
     }
     
-    //public Character clone(){
-        //Character newChar = new Character(Color bow, Image animal, boolean direction, ArrayList cards);
-        //return newChar
-    //}
+    public Character clone(){
+        Character newChar = new Character(bow, animal, direction, cards);
+        return newChar;
+    }
     
     public String toString(){
         return bow + "\t" + animal + "\t" + cards;
