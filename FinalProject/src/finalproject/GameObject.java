bb834b5c7e934d860ff1a55725d6188063c81233
harja <harja@10.16.129.223>
2021-04-06 11:26:16 -0400

    GameObject class default complete. Add update and mouseOver method with Aidan

--- FinalProject/src/finalproject/GameObject.java
+++ FinalProject/src/finalproject/GameObject.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+add update methods
+update the mouseOver method
  */
 package finalproject;
 
@@ -10,5 +9,95 @@ package finalproject;
  * @author harja
  */
 public class GameObject {
+    int xPos;
+    int yPos;
+    int rotation;
+    int width;
+    int height;
+    double scale;
+    boolean direction; //true == right
     
+    public GameObject(){
+        xPos = 0;
+        yPos = 0;
+        rotation = 0;
+        width = 0;
+        height = 0;
+        scale = 0.0;
+        direction = true; //Facing right
+    }
+    
+    public GameObject(int xPos, int yPos, int rotation, int width, int height, double scale, boolean direction){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.rotation = rotation;
+        this.width = width;
+        this.height = height;
+        this.scale = scale;
+        this.direction = direction;
+    }
+    
+    public void move(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+    public void setX(int xPos){
+        this.xPos = xPos;
+    }
+    public void setY(int yPos){
+        this.yPos = yPos;
+    }
+    public int getX(){
+        return xPos;
+    }
+    public int getY(){
+        return yPos;
+    }
+    
+    public void setRotation(int rotation){
+        this.rotation = rotation;
+    }
+    public int getRotation(){
+        return rotation;
+    }
+    
+    public void setScale(double scale){
+        this.scale = scale;
+    }
+    public double getScale(){
+        return scale;
+    }
+    
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public void setHeight(int height){
+        this.height = height;
+    }
+    public int getHeight(){
+        return height;
+    }
+    
+    public void setDirection(boolean direction){
+        this.direction = direction;
+    }
+    public boolean getDirection(){
+        return direction;
+    }
+    
+    public void mouseOver(){ //check if the mouse is over a object
+        
+    }
+    
+    public GameObject clone(){
+        GameObject newGameObject = new GameObject(xPos, yPos, rotation, width, height, scale, direction);
+        return newGameObject;
+    }
+    
+    public String toString(){
+        return "X Position: " + xPos + "\tY Position: " + yPos + "\tRotation: " + rotation + "\tWidth: " + width + "\tHeight: " + height + "\tScale: " + scale + "\tDirection: " + direction;
+    }
 }
