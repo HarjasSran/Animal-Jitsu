976cd217550f790bbc0a21218a0798e6bc40325b
harja <harja@10.16.129.223>
2021-04-06 15:31:47 -0400

    Player class almost complete. make second constructor work

---
+++ FinalProject/src/finalproject/Player.java
@@ -0,0 +1,43 @@
+/*
+
+ */
+package finalproject;
+
+/**
+ *
+ * @author harja
+ */
+public class Player extends Character{
+    String name;
+    int rank;
+    
+    public Player(){
+        name = "";
+        rank = 0;
+    }
+    
+    //public Player(String name, Color bow, Image animal, boolean direction, ArrayList cards){
+        
+    //}
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setRank(int rank){
+        this.rank = rank;
+    }
+    public int getRank(){
+        return rank;
+    }
+    
+    //clone
+    
+    public String toString(){
+        return name + "\t" + rank;
+    }
+}
