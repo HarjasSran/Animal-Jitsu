48ee450b19e856714a02e311d3ce74f61549113c
harja <harja@10.16.129.223>
2021-04-06 15:10:13 -0400

    pseudo code for particle class. need images

---
+++ FinalProject/src/finalproject/Particle.java
@@ -0,0 +1,25 @@
+/*
+Cant do yet, need to wait for images
+ */
+package finalproject;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author harja
+ */
+public class Particle extends GameObject {
+    Image element;
+    
+    //public Particle(){
+    //public Particle(image element, boolean direction)[    
+    
+    //setElement(Image element)
+    
+    //getElement()
+    
+    //clone()
+    
+    //toString()
+}
