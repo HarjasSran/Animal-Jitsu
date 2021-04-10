eefc4f41e4e7796eaf265aaf9a20b822a4779c35
harja <harja@10.16.129.223>
2021-04-10 16:28:50 -0400

    n

--- FinalProject/src/finalproject/User.java
+++ FinalProject/src/finalproject/User.java
@@ -12,9 +12,6 @@ import java.awt.Color;
 public class User {
     String name;
     int rank;
-    Color bow;
-    String date;
-    String animal;
     
     /**
      * Default User constructor
@@ -22,9 +19,7 @@ public class User {
     public User(){
         name = "";
         rank = 0;
-        bow = Color.WHITE;
-        date = "Jan 01 2000";
-        animal = "DUCK";
+
     }
     
     /**
@@ -35,12 +30,10 @@ public class User {
      * @param date - date user was created
      * @param animal - animal which user used
      */
-    public User(String name, int rank, Color bow, String date, String animal){
+    public User(String name, int rank){
         this.name = name;
         this.rank = rank;
-        this.bow = bow;
-        this.date = date;
-        this.animal = animal;
+
     }
     
     /**
@@ -72,58 +65,17 @@ public class User {
     public int getRank(){
         return rank;
     } 
+
     
-    /**
-     * Mutator method to set the color of the user's bow
-     * @param bow - color of the user's bow
-     */
-    public void setBow(Color bow){
-        this.bow = bow;
-    }
-    /**
-     * Accessor method for color of bow
-     * @return - color of user's bow
-     */
-    public Color getBow(){
-        return bow;
-    }
-    
-    /**
-     * Mutator method to set the date the user was created
-     * @param date - date the user was created
-     */
-    public void setDate(String date){
-        this.date = date;
-    }
-    /**
-     * Accessor method for date user was created
-     * @return - the date user was created
-     */
-    public String getDate(){
-        return date;
-    }
-    
-    /**
-     * Mutator method to set the animal which the user is using
-     * @param animal - animal that the user is using
-     */
-    public void setAnimal(String animal){
-        this.animal = animal;
-    }
-    /**
-     * Accessor method for animal that user is using
-     * @return - animal that the user is using
-     */
-    public String getAnimal(){
-        return animal;
-    }
+
+
     
     /**
      * Cloning method to clone the User 
      * @return - new cloned user
      */
     public User clone(){
-        User newUser = new User(name, rank, bow, date, animal); //Create new user with the exact same attributes and values as the other user
+        User newUser = new User(name, rank); //Create new user with the exact same attributes and values as the other user
         return newUser;
     }
     
@@ -132,6 +84,6 @@ public class User {
      * @return - all attributes and values of the user
      */
     public String toString(){
-        return "Name: " + name + "\tRank: " + rank + "\tBow Color: " + bow + "\tDate: " + date + "\tAnimal: " + animal;
+        return "Name: " + name + "\tRank: " + rank;
     }
 }
